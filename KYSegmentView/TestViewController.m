bb552449754c8d0022aab0ab62cee4d9302181ac
KangYi <kangyi_mm@163.com>
2017-12-18 14:09:05 +0800

    KYSegmentView

--- KYSegmentView/TestViewController.m
+++ KYSegmentView/TestViewController.m
@@ -33,6 +33,7 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
     return 0.01f;
 }
+
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
     return 0.01f;
 }
